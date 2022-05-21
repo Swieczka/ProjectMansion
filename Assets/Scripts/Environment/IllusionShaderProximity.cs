6d87900722a8f5b93e0b904dc9b75c93471f8514
MajsterEmski <92628963+MajsterEmski@users.noreply.github.com>
2022-05-21 18:43:42 +0200

    Kitchen Illusion script Tweak

--- Assets/Scripts/Environment/IllusionShaderProximity.cs
+++ Assets/Scripts/Environment/IllusionShaderProximity.cs
@@ -5,29 +5,49 @@ using UnityEngine;
 public class IllusionShaderProximity : MonoBehaviour
 {
     private Material material;
+    private MeshRenderer meshRenderer;
     [SerializeField][Range(0, 1)] private float distortion;
     [SerializeField][Range(0, 100)] private float distortionScale;
+    [SerializeField][Range(0, 2)] private float changeSpeedModificator;
 
     private void Start()
     {
-        material = GetComponent<MeshRenderer>().material;
+        meshRenderer = GetComponent<MeshRenderer>();
+        material = meshRenderer.material;
         material.SetFloat("Distortion", distortion);
         material.SetFloat("DistortionScale", distortionScale);
+        material.SetFloat("Opacity", 1);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        StopAllCoroutines();
         StartCoroutine(IllusionFadeAway());
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
+        StopAllCoroutines();
         StartCoroutine(IllusionFadeIn());
     }
 
     private IEnumerator IllusionFadeAway()
     {
-        while (material.GetFloat("Distortion") < 1)
+        meshRenderer.enabled = true;
+        float t = 0f;
+
+        while (t < 1)
+        {
+            material.SetFloat("Distortion", Mathf.Lerp(material.GetFloat("Distortion"), 1, t));
+            material.SetFloat("DistortionScale", Mathf.Lerp(material.GetFloat("DistortionScale"), 0, t));
+            material.SetFloat("Opacity", Mathf.Lerp(material.GetFloat("Opacity"), 0, t));
+            t += Time.deltaTime * changeSpeedModificator;
+            if (t >= 1) meshRenderer.enabled = false;
+            yield return null;
+        }
+       // meshRenderer.enabled = false;
+
+        /*while (material.GetFloat("Distortion") < 1)
         {
            material.SetFloat("Distortion", material.GetFloat("Distortion") + Time.deltaTime);
             yield return null;
@@ -36,13 +56,25 @@ public class IllusionShaderProximity : MonoBehaviour
         {
             material.SetFloat("DistortionScale", material.GetFloat("DistortionScale") - Time.deltaTime * 10f);
             yield return null;
-        }
-        
+        }*/
+
     }
 
     private IEnumerator IllusionFadeIn()
     {
-        while (material.GetFloat("Distortion") > distortion)
+        meshRenderer.enabled = true;
+        float t = 0f;
+
+        while (t < 1)
+        {
+            material.SetFloat("Distortion", Mathf.Lerp(material.GetFloat("Distortion"), distortion, t));
+            material.SetFloat("DistortionScale", Mathf.Lerp(material.GetFloat("DistortionScale"), distortionScale, t));
+            material.SetFloat("Opacity", Mathf.Lerp(material.GetFloat("Opacity"), 1, t));
+            t += Time.deltaTime * changeSpeedModificator;
+            yield return null;
+        }
+
+        /*while (material.GetFloat("Distortion") > distortion)
         {
             material.SetFloat("Distortion", material.GetFloat("Distortion") - Time.deltaTime);
             yield return null;
@@ -51,6 +83,6 @@ public class IllusionShaderProximity : MonoBehaviour
         {
             material.SetFloat("DistortionScale", material.GetFloat("DistortionScale") + Time.deltaTime * 10f);
             yield return null;
-        }
+        }*/
     }
 }
